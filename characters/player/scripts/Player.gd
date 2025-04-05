9257e4cfa080dc8bde48486579ea80db78f25dc4
oneilk10 <oneilk10@tcd.ie>
2025-04-05 15:16:45 +0200

    trying to animate the walking

--- characters/player/scripts/Player.gd
+++ characters/player/scripts/Player.gd
@@ -1,5 +1,13 @@
 class_name Player extends CharacterBody2D
 
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
+var state : String = "idle"
+
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sprite : Sprite2D = $Sprite2D
+
 @export var move_speed : float = 200.0
 
 func _ready():
@@ -7,7 +15,6 @@ func _ready():
 
 func _process(delta):
 	# Get input direction
-	var direction = Vector2.ZERO
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
@@ -18,5 +25,30 @@ func _process(delta):
 	# Set velocity directly (not multiplied by delta here)
 	velocity = direction * move_speed
 	
+	if SetState() == true:
+		UpdateAnimation()
+	
 func _physics_process(delta):
 	move_and_slide()
+
+func SetDirection() -> bool :
+	return true
+	
+func SetState() -> bool : 
+	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
+	if new_state == state:
+		return false
+	return true
+	
+func UpdateAnimation() -> void : 
+	animation_player.play(state + "_" + AnimDirection())
+	pass
+	
+func AnimDirection() -> String :
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "up"
+	else:
+		return "side"
+	
