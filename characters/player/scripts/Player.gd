a57f86c6c197f333689cce07a0424df0e8e2fd11
oneilk10 <oneilk10@tcd.ie>
2025-04-06 14:31:20 +0200

    player moves

--- characters/player/scripts/Player.gd
+++ characters/player/scripts/Player.gd
@@ -25,19 +25,32 @@ func _process(delta):
 	# Set velocity directly (not multiplied by delta here)
 	velocity = direction * move_speed
 	
-	if SetState() == true:
+	if SetState() == true || SetDirection() == true:
 		UpdateAnimation()
 	
 func _physics_process(delta):
 	move_and_slide()
 
 func SetDirection() -> bool :
+	var new_dir : Vector2 = cardinal_direction
+	if direction == Vector2.ZERO:
+		return false
+	if direction.y == 0:
+		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
+	elif direction.x == 0:
+		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
+		
+	if new_dir == cardinal_direction:
+		return false
+	cardinal_direction = new_dir
+	sprite.scale.x = -1 if cardinal_direction == Vector2.LEFT else 1
 	return true
 	
 func SetState() -> bool : 
 	var new_state : String = "idle" if direction == Vector2.ZERO else "walk"
 	if new_state == state:
 		return false
+	state = new_state
 	return true
 	
 func UpdateAnimation() -> void : 
